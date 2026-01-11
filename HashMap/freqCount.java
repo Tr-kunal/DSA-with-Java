e682403991e6266b3552518fe0586001a062212d
Kunal <kunalsinghal365@gmail.com>
2026-01-11 20:31:33 +0530

    Hashmap revision

---
+++ HashMap/freqCount.java
@@ -0,0 +1,28 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class freqCount {
+
+    public void frequency(int[] arr, int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+
+        freqCount fcount = new freqCount();
+
+        fcount.frequency(arr, n);
+    }
+}
